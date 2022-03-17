bc4595c3d181fde5075525612ab40e02bf6cf3d4
piriev <95006433+piriev@users.noreply.github.com>
2022-03-17 09:40:53 +0700

    Add files via upload

--- ggC.py
+++ ggC.py
@@ -5,14 +5,19 @@
 #veg.append(newVeg2)
 #print(veg)
 
-commands = ['игры','погода','новости','добавить','удалить', 'команды', 'выход']
+jobList = ['купить продукты','поставить вещи в стирку']
 command = input('Введите команду: ')
-while(command != commands[6]):
-    if command == commands[3]:
-        commands.append(input('Введите новую команду: '))
-    elif command == commands[4]:
-        commands.remove(input('Введите название команды: '))
-    elif command == commands[5]:
-        for elem in commands:
+while command != 'выход':
+    if command == 'добавить':
+        jobList.append(input('Введите новую заметку: '))
+    elif command == 'удалить':
+        jobList.remove(input('Введите название заметки: '))
+    elif command == 'редактировать':
+        name = input('Введите название заметки: ')
+        index = jobList.index(name)
+        if name in jobList:
+            jobList[index] = input('Введите новое название: ')
+    elif command == 'список':
+        for elem in jobList:
             print(elem)
     command = input('Введите команду: ')
