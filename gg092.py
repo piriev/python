d19c9985a8ffd3e3e2982745ea784e6f4fc9891e
piriev <95006433+piriev@users.noreply.github.com>
2022-05-19 09:39:12 +0700

    Add files via upload

--- gg092.py
+++ gg092.py
@@ -1,47 +1,55 @@
-pole =  [['O','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*'],
-         ['*','*','*','*','*','*','*','*','*','*']]
+pole =  [['O',' ',' ',' ','#','*','*','*','*','*'],
+         ['#','#','#',' ','#','*','*','*','*','*'],
+         ['*','*','#',' ','#','*','*','*','*','*'],
+         ['*','*','#',' ','#','*','*','*','*','*'],
+         ['#','#','#',' ','#','*','*','*','*','*'],
+         [' ',' ',' ',' ','#','#','#','#','#','#'],
+         [' ','#','#','#','#','#',' ',' ',' ',' '],
+         [' ','#','#','#','#','#',' ','#','#',' '],
+         [' ',' ',' ',' ',' ',' ',' ','#','#',' '],
+         ['#','#','#','#','#','#','#','#','#','!']]
 for stroka in pole:
     for kletka in stroka:
         print(kletka,'',end='')
     print()
 placeA = 0
 placeB = 0
-while True:
-    pole[placeB][placeA] = '*'
+while pole[9][9] != 'O':
     placeNew = input('Введите команду: ')
     if placeNew == 'right':
         if placeA < 9:
-            placeA += 1
-            pole[placeB][placeA] = 'O'
-        else:
-            placeNew = input('Нельзя направо! Повторите попытку: ')
+            if pole[placeB][placeA + 1] != '#': 
+                pole[placeB][placeA] = ' '
+                placeA += 1
+                pole[placeB][placeA] = 'O'
+            else:
+                print('Нельзя направо!')
     if placeNew == 'left':
         if placeA > 0:
-            placeA -= 1
-            pole[placeB][placeA] = 'O'
-        else:
-            placeNew = input('Нельзя налево! Повторите попытку: ')
+            if pole[placeB][placeA - 1] != '#':
+                pole[placeB][placeA] = ' '
+                placeA -= 1
+                pole[placeB][placeA] = 'O'
+            else:
+                print('Нельзя налево!')
     if placeNew == 'up':
         if placeB > 0:
-            placeB -= 1
-            pole[placeB][placeA] = 'O'
-        else:
-            placeNew = input('Нельзя на вверх! Повторите попытку: ')
+            if pole[placeB - 1][placeA] != '#':
+                pole[placeB][placeA] = ' '
+                placeB -= 1
+                pole[placeB][placeA] = 'O'
+            else:
+                print('Нельзя на вверх!')
     if placeNew == 'down':
         if placeB < 9:
-            placeB += 1
-            pole[placeB][placeA] = 'O'
-        else:
-            placeNew = input('Нельзя вниз! Повторите попытку: ')
+            if pole[placeB + 1][placeA] != '#':
+                pole[placeB][placeA] = ' '
+                placeB += 1
+                pole[placeB][placeA] = 'O'
+            else:
+                 print('Нельзя вниз!')
     for stroka in pole:
         for kletka in stroka:
             print(kletka,'',end='')
         print()
+print('Победа!')
